9777a7ca3ce031db9e4a512c1bd427a85b88f1e2
passenger3e <ali_vara_01@hotmail.com>
2022-10-25 16:01:28 +0300

    Initial Commit

---
+++ src/BasicConseptAndVariables/P02_TaxAmount.java
@@ -0,0 +1,35 @@
+package BasicConseptAndVariables;
+
+import java.util.Scanner;
+
+public class P02_TaxAmount {
+    public static void main(String[] args) {
+        /*
+        KDV Tutarı Hesaplayan Program
+      Java ile kullanıcıdan alınan para değerinin KDV'li fiyatını ve KDV tutarını hesaplayıp ekrana bastıran programı yazın.
+        (Not : KDV tutarını 18% olarak alın)
+        KDV'siz Fiyat = 10;
+        KDV'li Fiyat = 11.8;
+        KDV tutarı = 1.8;
+        Odev
+        Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 , tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.
+         */
+        System.out.println("This tool is to calculate the Tax Amount and Total Amount");
+        Scanner getMoney=new Scanner(System.in);
+        double sumOfMoney;
+        double taxAmount;
+        double taxPercentage=(18.0/100);
+        System.out.println("|Description                    | Amount |");
+        System.out.println("------------------------------------------");
+
+        System.out.print("Please enter the Amount:         ");
+        sumOfMoney=getMoney.nextDouble();
+        System.out.println("------------------------------------------");
+        taxAmount=sumOfMoney*taxPercentage;
+        System.out.println("Amount without tax is:           " +taxPercentage);
+        System.out.println("------------------------------------------");
+        System.out.println("Tax Amount is:                   " +taxAmount +
+                "\n------------------------------------------\nAnd Total Amount with tax is:    "+(taxAmount+sumOfMoney));
+
+    }
+}
