0c80330d04046b30629958d964d892401083b42c
passenger3e <ali_vara_01@hotmail.com>
2022-10-26 09:51:37 +0300

    Initial Commit

--- src/BasicConseptAndVariables/P05_Area_CircumOfCircle.java
+++ src/BasicConseptAndVariables/P05_Area_CircumOfCircle.java
@@ -20,11 +20,17 @@ public class P05_Area_CircumOfCircle {
         double areaCircle;
         double circumferenceCircle;
         double pi=3.14;
+        int angle;
+        double sectorArea;
         System.out.print("Please enter the radius of the circle to calculate Area and Circumference of the circle: ");
         radius=getRadius.nextInt();
+        System.out.print("Please enter central angle of the Circle to calculate the sector area of the slice: ");
+        angle=getRadius.nextInt();
         circumferenceCircle=2*pi*radius;
         //areaCircle=pi*radius*radius;
         areaCircle= (BigDecimal.valueOf(pi).multiply(BigDecimal.valueOf(radius))).multiply(BigDecimal.valueOf(radius)).doubleValue();
+        sectorArea=(pi*(radius*radius)*angle)/360;
         System.out.println("Area of the Circle is: "+areaCircle+"\nand Circumference of the Circle is: "+circumferenceCircle);
+        System.out.println("Sector area of the given angle on Circle: "+sectorArea);
     }
 }
