2b8d5479950d72a2293c8d41da63b114ba2df268
passenger3e <ali_vara_01@hotmail.com>
2022-10-29 21:51:17 +0300

    sorting 3 number with if-else

--- src/ConditionalStatementsAndBlocks/SortingThreeNumbers.java
+++ src/ConditionalStatementsAndBlocks/SortingThreeNumbers.java
@@ -13,17 +13,21 @@ public class SortingThreeNumbers {
         System.out.print("Enter 3. number: ");
         num3=getNumber.nextInt();
         if(num1<num2&&num1<num3){
-            System.out.print(num1+" < ");
+            System.out.print(num1+"<");
             if(num2<num3){
-                System.out.print(num2+" < "+num3);
-            }else System.out.print(num3+" < "+num2);
+                System.out.print(num2+"<"+num3);
+            }else System.out.print(num3+(num2==num3?"=":"<")+num2);
         }else if(num2<num3){
-            System.out.print(num2+"<");
-            System.out.print(num1<num3?(num1+"<"+num3):(num3+"<"+num1));
+            System.out.print(num2);
+            System.out.print(num1==num2?"=":"<");
+            System.out.print(num1==num3?(num1+"="+num3):(num1<num3?num1+"<"+num3:num3+"<"+num1));
         }else if(num3<num2)
         {
-            System.out.print(num3+"<");
-            System.out.print(num1<num2?(num1+"<"+num2):(num2+"<"+num1));
-        }else System.out.print(num1+"="+ num2+ "="+num3);
+            System.out.print(num3);
+            System.out.print(num1==num3?"=":"<");
+            System.out.print(num1==num2?(num1+"="+num2)
+                    :(num1<num2?(num1+"<"+num2):num2+"<"+num1));
+
+        }else System.out.print(num1>num2?num2+"="+num3+"<"+num1:num1+"="+num2+ "="+num3);
     }
 }
