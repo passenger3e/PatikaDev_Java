372d9c97a751c5d173e8b709e6c4bdd7ab116405
passenger3e <ali_vara_01@hotmail.com>
2022-10-29 20:28:17 +0300

    Initial Commit

---
+++ src/ConditionalStatementsAndBlocks/ActivitySugesstion.java
@@ -0,0 +1,21 @@
+package ConditionalStatementsAndBlocks;
+
+import java.util.Scanner;
+
+public class ActivitySugesstion {
+    public static void main(String[] args) {
+        int temp;
+        Scanner getTemp=new Scanner(System.in);
+        System.out.print("Please enter Temperature: ");
+        temp=getTemp.nextInt();
+        if(temp<=5)
+            System.out.println("You can go Skiing.");
+        if(temp>5&&temp<=15)
+            System.out.println("You can go to Cinema.");
+        if(temp>10&&temp<=25)
+            System.out.println("You can go to Picnic.");
+        if(temp>25)
+            System.out.println("You can go to Swimming.");
+
+    }
+}
