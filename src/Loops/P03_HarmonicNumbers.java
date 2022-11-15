e1e451a6bd4a5730c07969eeb093b9c28ccd8ca8
passenger3e <ali_vara_01@hotmail.com>
2022-11-15 17:29:50 +0300

    Initial Commit

--- src/Loops/P03_HarmonicNumbers.java
+++ src/Loops/P03_HarmonicNumbers.java
@@ -6,9 +6,9 @@ public class P03_HarmonicNumbers {
     public static void main(String[] args) {
         Scanner getValue=new Scanner(System.in);
         double total=0;
-        int i=1,value=0;
+        int i,value=0;
         while (value<=0){
-            System.out.print("Please Enter an Integer value: ");
+            System.out.print("Please Enter a positive Integer and bigger than zero: ");
             value=getValue.nextInt();
         }
         for(i=1;i<=value;i++){
