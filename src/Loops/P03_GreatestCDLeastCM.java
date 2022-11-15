0556909398f33b1a83771675d10c2f6306d08ecf
passenger3e <ali_vara_01@hotmail.com>
2022-11-15 21:51:36 +0300

    Initial Commit

--- src/Loops/P03_GreatestCDLeastCM.java
+++ src/Loops/P03_GreatestCDLeastCM.java
@@ -1,6 +1,8 @@
+package Loops;
+
 import java.util.Scanner;
 
-public class GreatestCDLeastCM {
+public class P03_GreatestCDLeastCM {
     public static void main(String[] args) {
         Scanner getNumber = new Scanner(System.in);
 
@@ -10,7 +12,7 @@ public class GreatestCDLeastCM {
         num1 = getNumber.nextInt();
         System.out.print("Enter second number: ");
         num2 = getNumber.nextInt();
-        if (num1 < num2) {
+        if (num1 < num2) { //find the smallest one and assign it to the dnum1.
             dnum1 = num1;
             dnum2 = num2;
         } else if (num1 > num2) {
@@ -20,21 +22,24 @@ public class GreatestCDLeastCM {
             dnum1=num1;
             dnum2=num2;
         }
-        int k = 2,l=0;
-        while (k <=dnum1) {
+        int k = 2;//,l=0;
+        while (k <=dnum1) { //using the smallest one
             while (num1 % k == 0 || num2 % k == 0) {
                 if (num1 % k == 0 && num2 % k == 0) greatestCD *= k;
                 leastCM *= k;
                 if (num1 % k == 0) num1 /= k;
 
                 if (num2 % k == 0) num2 /= k;
-                l++;
+                //l++;
             }
-            if (k == 2) k++;
-            else k += 2;
+            if (k == 2) k++;//only increase by 1 if this is starting point which is 2
+            else k += 2; //else condition no need to increase by one start to do that by 2. to check dividers.
            // k++;
         }
         System.out.println("Greatest Common divisor: " + greatestCD);
         System.out.println("Least Common Multiple: " + leastCM * num2 * num1);
+        //Formalized version of LCM
+        leastCM=(dnum1*dnum2)/greatestCD;//formula of the LCM by using the GCD.
+        System.out.println("Other way to calculate the Least Common Multiple: "+leastCM);
     }
 }
