665a160f2e620629df30fb833bdf076c7bd78030
passenger3e <ali_vara_01@hotmail.com>
2022-11-16 12:10:47 +0300

    Initial Commit

---
+++ src/Loops/P03_MinMax.java
@@ -0,0 +1,29 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class P03_MinMax {
+    public static void main(String[] args) {
+        Scanner getValue=new Scanner(System.in);
+        int value,minValue=0,maxValue=0,count,i=1;
+        System.out.print("PHow many numbers will you enter: ");
+        count=getValue.nextInt();
+        while(i!=(count+1)){
+            System.out.print("Please enter "+i+". number: ");
+            if(i==1){
+                minValue=getValue.nextInt();
+            }
+            else {
+                value=getValue.nextInt();
+                if(value<minValue){
+                    minValue=value;
+                }else maxValue=value;
+            }
+            i++;
+
+        }
+        System.out.println("Minimum value is: "+minValue);
+        System.out.println("Maximum value is: "+maxValue);
+
+    }
+}
