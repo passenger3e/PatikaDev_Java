c3525c0529b103679967ff1da3baf2b794002e34
passenger3e <ali_vara_01@hotmail.com>
2022-11-16 16:59:41 +0300

    Initial Commit

--- src/Loops/P03_Fibonacci.java
+++ src/Loops/P03_Fibonacci.java
@@ -8,6 +8,7 @@ public class P03_Fibonacci {
         System.out.print("Please enter how many elements will be included in Fibonacci series: ");
         int count=getCount.nextInt();
         int firstNumber=0,secondNumber=1,resultNumber;
+        System.out.print(count+" elements Fibonacci series  are: ");
         System.out.print(0+", ");
         for(int i=1;i<=count;i++){
             resultNumber=firstNumber+secondNumber;
@@ -15,5 +16,6 @@ public class P03_Fibonacci {
             secondNumber=resultNumber;
             System.out.print(firstNumber+", ");
         }
+        System.out.println("\b\b");
     }
 }
