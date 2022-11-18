07127106c24385e91dfb5e2a572bb584759d2fef
passenger3e <ali_vara_01@hotmail.com>
2022-11-18 11:41:58 +0300

    Initial Commit

--- src/Methods/P04_ExponentialNumbersRecursive.java
+++ src/Methods/P04_ExponentialNumbersRecursive.java
@@ -6,11 +6,14 @@ public class P04_ExponentialNumbersRecursive {
     public static void main(String[] args) {
 
         int base,powerOf;
-        System.out.print("Please enter base number: ");
-        base=getNumber.nextInt();
-        System.out.print("Please enter the power: ");
-        powerOf=getNumber.nextInt();
-        System.out.println(GetExponential(base,powerOf));
+        do {
+            System.out.print("Please enter base number: ");
+            base = getNumber.nextInt();
+            if(base<0) break;
+            System.out.print("Please enter the power: ");
+            powerOf = getNumber.nextInt();
+            System.out.println("Result is: "+GetExponential(base, powerOf));
+        }while(true);
 
     }
     public static double GetExponential(int b,int p){
